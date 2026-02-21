1817596df286188988da50c748aa48a29ee6881d
Rajeev Bhuvaneswaran <rajeevbiz@gmail.com>
2026-02-21 09:52:08 -0500

    Updated gear ratio

--- src/main/java/com/adambots/Constants.java
+++ src/main/java/com/adambots/Constants.java
@@ -67,7 +67,7 @@ public final class Constants {
         // Total ratio = stage1 * stage2 (motor rotations per mechanism rotation)
         // TODO: Get actual values from mech team
         public static final double kArmPlanetaryRatio = 20.0;  // e.g., 5.0 for 5:1 planetary
-        public static final double kArmBeltRatio = 1.4;        // e.g., 2.0 for 36T:18T belt
+        public static final double kArmBeltRatio = 2.0;        // e.g., 2.0 for 36T:18T belt
         public static final double kArmTotalGearRatio = kArmPlanetaryRatio * kArmBeltRatio;
 
         public static final double kLowSpeed = 0.3;
